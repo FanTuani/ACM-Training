b1332325fef7b5a57571ca3822e370b82f34325a
RiceQuakes <2486921393@qq.com>
2024-08-01 10:18:34 +0800

    Update 莫队查询区间不同数的个数.cpp

--- Templates/莫队查询区间不同数的个数.cpp
+++ Templates/莫队查询区间不同数的个数.cpp
@@ -1,8 +1,8 @@
-// 莫队查询区间不同数的个数 P3901
+// 莫队查询区间不同数的个数 SP3267
 #include <bits/stdc++.h>
 #define int long long
 using namespace std;
-const int N = 2e5 + 10, INF = 0x3f3f3f3f3f3f3f;
+const int N = 2e6 + 10, INF = 0x3f3f3f3f3f3f3f;
 int a[N], n, q, bel[N];
 struct Q {
     int l, r, idx;
@@ -41,13 +41,14 @@ void del(int x) {
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    cin >> n >> q;
+    cin >> n;
     for (int i = 1; i <= n; i++) {
         cin >> a[i];
     }
     prework();
     vector<Q> qs;
     int cnt = 0;
+    cin >> q;
     while (q--) {
         int l, r;
         cin >> l >> r;
@@ -69,16 +70,9 @@ signed main() {
             del(r--);
         }
         // ans: 区间不同数的个数
-        if (ans < R - L + 1) {
-            out[idx] = 0;
-        } else {
-            out[idx] = 1;
-        }
+        out[idx] = ans;
     }
     for (int i = 1; i <= cnt; i++) {
-        if (out[i])
-            cout << "Yes\n";
-        else
-            cout << "No\n";
+        cout << out[i] << '\n';
     }
 }

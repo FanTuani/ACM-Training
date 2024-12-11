7e43340c1211a403590f9c8874a39019a6418c05
RiceQuakes <2486921393@qq.com>
2024-12-11 15:27:59 +0800

    Create 组合数.cpp

---
+++ Templates/组合数.cpp
@@ -0,0 +1,10 @@
+int mod = 998244353;
+
+int cres[67][67];
+int C(int n, int m) {
+    if (m == 0 || m == n)
+        return 1;
+    if (cres[n][m])
+        return cres[n][m] % mod;
+    return cres[n][m] = (C(n - 1, m) + C(n - 1, m - 1)) % mod;
+}

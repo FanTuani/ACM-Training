47f7082198ce87bd11e1e9c9196e5779f861ea3f
RiceQuakes <2486921393@qq.com>
2024-12-24 22:19:23 +0800

    Create 质因数分解+记录质因数出现次数.cpp

---
+++ Templates/质因数分解+记录质因数出现次数.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+map<int, int> mp;
+int calc(int x) {
+    int x;
+    cin >> x;
+    for (int fac = 2; fac * fac <= x; fac++) {
+        while (x % fac == 0) {
+            x /= fac;
+            mp[fac]++;
+        }
+    }
+    if (x > 1) {
+        mp[x]++;
+    }
+}
+// sqrt(x) * log(x)
